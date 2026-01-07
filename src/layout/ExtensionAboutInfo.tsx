60d6860283c868dd9cc63855e382e1c079fe7305
Aviad Pineles <paviad2@gmail.com>
2026-01-08 01:22:24 +0200

    chore: published version 4.1.0

--- src/layout/ExtensionAboutInfo.tsx
+++ src/layout/ExtensionAboutInfo.tsx
@@ -1,3 +1,3 @@
 export const EXTENSION_NAME = 'ElvenAssist';
-export const EXTENSION_VERSION = '4.0.0';
-export const EXTENSION_DATE = '5-Jan-2026';
+export const EXTENSION_VERSION = '4.1.0';
+export const EXTENSION_DATE = '8-Jan-2026';
