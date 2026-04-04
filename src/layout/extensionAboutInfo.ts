9819bbc69269ee66278fa038ec07e1f542618eba
Aviad Pineles <paviad2@gmail.com>
2026-04-04 16:17:52 +0300

    chore: bump version to 7.2.0

--- src/layout/extensionAboutInfo.ts
+++ src/layout/extensionAboutInfo.ts
@@ -1,3 +1,3 @@
 export const EXTENSION_NAME = 'ElvenAssist';
-export const EXTENSION_VERSION = '7.1.2';
-export const EXTENSION_DATE = '16-Mar-2026';
+export const EXTENSION_VERSION = '7.2.0';
+export const EXTENSION_DATE = '4-Apr-2026';
