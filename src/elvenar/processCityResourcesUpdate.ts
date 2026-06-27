6b785533bbd21fc0f36c8c5c6c2eb91a62140a18
Aviad Pineles <paviad2@gmail.com>
2026-06-27 11:20:36 +0300

    fix: detect badge expenditure in real time

--- src/elvenar/processCityResourcesUpdate.ts
+++ src/elvenar/processCityResourcesUpdate.ts
@@ -1,3 +1,4 @@
+import { Badges, Relics } from '../model/badges';
 import { ExtensionSharedInfo } from '../model/extensionSharedInfo';
 import { getAccountBySessionId } from './AccountManager';
 
@@ -15,11 +16,42 @@ export const processCityResourcesUpdate = async (untypedJson: unknown, sharedInf
 
   const cityResources = json?.responseData as
     | {
-        resources: Record<string, number>;
-      }
+      resources: Record<string, number>;
+    }
     | undefined;
 
   const { __class__, ...resources } = cityResources?.resources || {};
+
+  const badges: Badges = {
+    badge_brewery: resources.badge_brewery,
+    badge_carpenters: resources.badge_carpenters,
+    badge_farmers: resources.badge_farmers,
+    badge_blacksmith: resources.badge_blacksmith,
+    golden_bracelet: resources.golden_bracelet,
+    diamond_necklace: resources.diamond_necklace,
+    elegant_statue: resources.elegant_statue,
+    witch_hat: resources.witch_hat,
+    druid_staff: resources.druid_staff,
+    badge_wonderhelper: resources.badge_wonderhelper,
+    badge_unit: resources.badge_unit,
+    money_sack: resources.money_sack,
+    arcane_residue: resources.arcane_residue,
+    recycled_potion: resources.recycled_potion,
+    enchanted_tiara: resources.enchanted_tiara,
+    ghost_in_a_bottle: resources.ghost_in_a_bottle,
+  };
+
+  const relics: Relics = {
+    relic_crystal: resources.relic_crystal,
+    relic_elixir: resources.relic_elixir,
+    relic_gems: resources.relic_gems,
+    relic_magic_dust: resources.relic_magic_dust,
+    relic_marble: resources.relic_marble,
+    relic_planks: resources.relic_planks,
+    relic_scrolls: resources.relic_scrolls,
+    relic_silk: resources.relic_silk,
+    relic_steel: resources.relic_steel,
+  };
 
   const accountData = getAccountBySessionId(sharedInfo.sessionId);
   if (accountData?.cityQuery) {
