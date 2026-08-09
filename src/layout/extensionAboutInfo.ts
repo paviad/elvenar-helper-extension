2bdc8ea63f5ee097b90740d2fd1282af5c2d2756
Aviad Pineles <paviad2@gmail.com>
2026-08-09 14:34:11 +0300

    chore: publish 10.1.0

--- src/layout/extensionAboutInfo.ts
+++ src/layout/extensionAboutInfo.ts
@@ -1,3 +1,3 @@
 export const EXTENSION_NAME = 'ElvenAssist';
-export const EXTENSION_VERSION = '10.0.0';
-export const EXTENSION_DATE = '6-Aug-2026';
+export const EXTENSION_VERSION = '10.1.0';
+export const EXTENSION_DATE = '9-Aug-2026';
